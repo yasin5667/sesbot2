29c0af7f04042e4570b89d4d69a37e2652d33cff
yasin5667 <141853863+yasin5667@users.noreply.github.com>
2023-08-12 15:45:47 +0300

    Add files via upload

---
+++ Welcome/beş_config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  token:"MTEzOTIxNjgzMDIxMzM5ODYxOA.GfYOOV.SJWqUlNs9EBrOykoi16pznQ4kSJKcQ-BxR2a0c", //Bot Tokeniniz => Bot Oluşturduktan Sonra Intentsleri Açmayı Unutma!
+  guildID:"1136389993447825499", //Kullanıcağın Sunucunun ID'si
+  presence: "", // Bot Durumu
+  guildID: "", // Sunucu ID'si
+  mp3: true, // mp3 mü Yoksa Youtube Üzerindenmi Çalmasını İsterseniz Burdan true veya false Yazın.
+  file: "./ses.mp3", // mp3'ü Seçtiyseniz Ses Dosyasının Uzantısı
+  staffRole: "",//En Alt Yetkili Rol ID'si
+  youtubeURL: "https://www.youtube.com/watch?v=pQ_T0EiWccM", // Youtube'u Seçtiyseniz Bir Youtube URL'si
+  tokens: [ // Tokenler ["55555","5555"] Şeklinde Giriniz.
+    "",
+    
+  ],
+  channels: [ // Welcome Kanallarının ID'leri ["55555","5555"] Şeklinde Giriniz.
+    "",
+    
+  ]
+}
